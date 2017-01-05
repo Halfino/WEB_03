8b181ad831f7a36eeca61ccaeb1864d39903cfb8
Halfino <jan.k.koranda@gmail.com>
2017-01-05 18:28:23 +0100

    Some typo in menus at index created array of articles in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,11 @@
 class PagesController < ApplicationController
   def index
+    @sideArticles = Article.where(category_id: 2).where(published: "t")
+    @mainArticles = Article.where(category_id: 1).where(published: "t")
   end
 
   def sitters
+    @sideArticles = Article.where(category_id: 2).where(published: "t")
+    @mainArticles = Article.where(category_id: 1).where(published: "t")
   end
 end
