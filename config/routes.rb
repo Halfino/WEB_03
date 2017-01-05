7598d72bc2013bf00f2767e2daa0ef5248110313
Halfino <jan.k.koranda@gmail.com>
2017-01-05 18:22:43 +0100

    created article categories via console installed devise and prepared rails app in development.rb and application.html.erb setup mainpage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :articles
+  root to: 'pages#index'
   get 'pages/index'
-
   get 'pages/sitters'
 
   # The priority is based upon order of creation: first created -> highest priority.
