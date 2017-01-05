b69f8f7fbf35a207de4f843d900aa506581e54dd
Halfino <jan.k.koranda@gmail.com>
2017-01-05 22:44:00 +0100

    added default image to mainpage when user has no image uploaded

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -17,11 +17,11 @@ class ImageUploader < CarrierWave::Uploader::Base
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
-   def default_url
+   def default_url(*args)
   #   # For Rails 3.1+ asset pipeline compatibility:
   #   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
   #
-     "/images/" + [version_name, "default.png"].compact.join('_')
+     "/assets/" + [version_name, "rsz_default.png"].compact.join('_')
    end
 
   # Process files as they are uploaded:
