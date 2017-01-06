a8ebd5f0f09c5ec6d4e5366359e93ea75da97308
Halfino <jan.k.koranda@gmail.com>
2017-01-06 17:31:21 +0100

    minor bug fixes, cz phone regexp udpated(missing checking numbers starting by 601-608. allowed also 600 and 609

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,11 @@ class User < ActiveRecord::Base
             format: {with: /\A(\d[8-9])|\A([2-9]\d)/, message: "Vek musi byt v rozmezi 18 - 99 let"}
   validates :description, presence: {message: "Toto pole nemuze byt prazdne."}, length: 1..500
   validates :city, presence: {message: "Toto pole nemuze byt prazdne"}
-  validates :phone, presence: {message: "Toto pole nemuze byt prazdne"}, format: {with: /\A([+][4][2][0]([6]|[7])
-            (\d{8}))|([6]|[7])(\d{8})/, message: "Spatny format telefonniho cisla. Spravny format je +420(7/6)xxxxxxxx
-             nebo (7/6)xxxxxxxx "}
-  validates :email, presence: {message: "Toto pole nemuze byt prazdne"},
+  validates :phone, presence: {message: "Toto pole nemuze byt prazdne"},
+            format: {with: /(\A([+][4][2][0](([6][0])|([7][2-7])([0-9]{7}))$))|(\A((([6][0])|([7][2-7]))([0-9]{7})$))/x,
+                     message: "Spatny format telefonniho cisla. Mozno pouzit pouze ceske mobilni cislo.
+                    Spravny format je (+420)tel.c."}
+  validates :email, presence: {message: "Toto pole nemuze byt prazdne"},length: 1..254,
             format: {with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+(\w{2,3})\z/i, message: "Neplatny email"}
   devise :database_authenticatable, :registerable,:authentication_keys => [:username]
   mount_uploader :image, ImageUploader
