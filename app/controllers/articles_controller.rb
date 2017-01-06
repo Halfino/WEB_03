4b4dc0e3908f514d022ef065967d3e311d5fad96
Halfino <jan.k.koranda@gmail.com>
2017-01-06 01:43:17 +0100

    preparing article administration

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
-  validates :title, presence: {message: "Toto pole nemuze byt prazdne" }
-  validates :text, presence: {message: "Toto pole nemuze byt prazdne"}
+
   # GET /articles
   # GET /articles.json
   def index
@@ -70,6 +69,6 @@ class ArticlesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def article_params
-      params.require(:article).permit(:title, :text, :published, :created_at)
+      params.require(:article).permit(:title, :text,:category_id, :published, :created_at)
     end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,9 @@
 class Article < ActiveRecord::Base
   belongs_to :category
+
+  validates_inclusion_of :published, :in => [true, false]
+  validates :title, presence: {message: "Titulek musi byt vyplnen"}
+  validates :text, presence: {message: "Text musi byt vyplnen"}
+
 end
+
